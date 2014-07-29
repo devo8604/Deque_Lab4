1d476ccd05ae70a410dc018f2063afe636ebeb0a
devo8604 <devo8604@gmail.com>
2014-07-28 19:41:14 -0600

    Initial Commit

---
+++ Deque_Lab4/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Deque_Lab4
+//
+//  Created by Devon Smith on 7/28/14.
+//  Copyright (c) 2014 Devon Smith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
